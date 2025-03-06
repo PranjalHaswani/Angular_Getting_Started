b7a7e7d176fabf1d33643df259a5aa67ccdcf385
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-03-06 10:43:46 +0530

    [Pranjal_Haswani]add. Display BridgeLabz logo using Property Binding Technique.

--- MyAngularApp/src/app/app.component.ts
+++ MyAngularApp/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyAngularApp';
+  message: string = "Hello from BridgeLabz ";  // Define message property
 }
